dcabfedcc7337523489ef5e9cd8b374633e0a918
Rachel-Oei <oeirachel@yahoo.com>
2025-07-17 07:22:35 +0000

    Fetch asset class and sector from yfinance

--- models.py
+++ models.py
@@ -1,24 +1,26 @@
 import yfinance as yf
 
 class Asset:
-    def __init__(self, ticker, sector, asset_class, quantity, purchase_price):
+    def __init__(self, ticker, quantity, purchase_price):
         self.ticker = ticker
-        self.sector = sector
-        self.asset_class = asset_class
         self.quantity = quantity
         self.purchase_price = purchase_price
-        self.adj_close = None
+        self.close = None
+
+        info = yf.Ticker(ticker).info
+        self.asset_class = info.get("quoteType", "UNKNOWN")
+        self.sector = info.get("sector", "UNKNOWN")
 
     def update_adjusted_close(self):
         data = yf.Ticker(self.ticker)
         hist = data.history(period="max")  
-        adj_close_series = hist['Close']     
-        self.adj_close = adj_close_series.iloc[-1] 
+        close_series = hist['Close']     
+        self.close = close_series.iloc[-1] 
         
     def transaction_value(self):
         return self.quantity * self.purchase_price
 
     def current_value(self):
-        if self.adj_close is None:
+        if self.close is None:
             return None
-        return self.quantity * self.adj_close
+        return self.quantity * self.close
