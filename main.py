a01868e440a95d49e4a1cb814fcafe7bf2b454f3
Rachel-Oei <oeirachel@yahoo.com>
2025-07-18 01:16:36 +0000

    Clean code by moving portfolio elements from main to class in models

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
 from models import Asset, Portfolio
 
-def add_assets(portfolio):
-    while True:
-        choice = input("Menu: \n1. Add asset \n2. View portfolio").lower()
-        if choice == '1':
-            ticker = input("Enter the ticker: ").upper()
-            quantity = int(input("Enter the quantity: "))
-            purchase_price = float(input("Enter the purchase price (USD): "))
-            asset = Asset(ticker, quantity, purchase_price)
-            asset.update_close()
-            portfolio.add_asset(asset)
-
-            print("\nCurrent Portfolio Overview:")
-            print(f"{'Ticker':<10} {'Asset Class':<15} {'Sector':<30} {'Mkt Cap':<10} {'Qty':<5} {'Purchase Price':<15} {'Current Price':<15} {'Daily Return %':<15}")
-            print("-" * 115)
-
-            for a in portfolio.assets:
-                market_cap = f"${int(a.market_cap / 1_000_000_000)}B" if isinstance(a.market_cap, (int, float)) else "N/A"
-                current_price = f"${a.close:.2f}" if a.close else "N/A"
-                daily_return = f"{a.daily_return:.2f}%" if a.daily_return is not None else "N/A"
-                print(f"{a.ticker:<10} {a.asset_class:<15} {a.sector:<30} {market_cap:<10} {a.quantity:<5} ${a.purchase_price:<14.2f} {current_price:<15} {daily_return:<15}")
-            print()
-
-            def print_weight_table(title, weight_dict):
+def print_weight_table(title, weight_dict):
                 print(f"\n{title}")
                 print(f"{'Name':<25} {'Weight %':<10} {'Bar':<50}")
                 print("-" * 85)
@@ -34,7 +12,18 @@ def add_assets(portfolio):
                     color = colors[i % 2]  # alternate color per row
                     bar = '█' * int(weight * 50)
                     print(f"{name:<25} {weight * 100:>6.2f}%   {color}{bar}{reset}")
-            
+
+def add_assets(portfolio):
+    while True:
+        choice = input("Menu: \n1. Add asset \n2. View portfolio").lower()
+        if choice == '1':
+            ticker = input("Enter the ticker: ").upper()
+            quantity = int(input("Enter the quantity: "))
+            purchase_price = float(input("Enter the purchase price (USD): "))
+            asset = Asset(ticker, quantity, purchase_price)
+            asset.update_close()
+            portfolio.add_asset(asset)
+            portfolio.print_asset_table()
             print_weight_table("Weights by Asset", portfolio.weights())
             print_weight_table("Weights by Asset Class", portfolio.weights_by_asset_class())
             print_weight_table("Weights by Sector", portfolio.weights_by_sector())
