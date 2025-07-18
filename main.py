46ba3a281a918f023975ca91021f3681256f5916
Rachel-Oei <oeirachel@yahoo.com>
2025-07-18 03:42:47 +0000

    Add historical price graph function and file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models import Asset, Portfolio
-from view import print_asset_table, print_weight_table
+from view import print_asset_table, print_weight_table, plot_multiple_assets
 
 def main():
     portfolio = Portfolio()
@@ -7,8 +7,9 @@ def main():
     while True:
         print("\nWhat do you wish to do?")
         print("1. Add an asset")
-        print("2. View portfolio summary")
-        print("3. Exit")
+        print("2. View historical prices")
+        print("3. View portfolio summary")
+        print("4. Exit")
 
         try:
             choice = int(input("Enter your choice: "))
@@ -20,12 +21,14 @@ def main():
             case 1:
                 portfolio.add_assets()
             case 2:
+                plot_multiple_assets(portfolio.assets, period="20y", interval="1d")
+            case 3:
                 print_asset_table(portfolio)
                 print_weight_table("Weights by Asset", portfolio.weights())
                 print_weight_table("Weights by Asset Class", portfolio.weights_by_asset_class())
                 print_weight_table("Weights by Sector", portfolio.weights_by_sector())
                 portfolio.summary()
-            case 3:
+            case 4:
                 print("Thanks for using the Portfolio Tracker. Goodbye!")
                 break
             case _:
