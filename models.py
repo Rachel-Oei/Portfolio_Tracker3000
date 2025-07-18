c4527c23a6847368caf248fa0d967db60f62571a
Rachel-Oei <oeirachel@yahoo.com>
2025-07-18 01:16:59 +0000

    Add class objects from main to models

--- models.py
+++ models.py
@@ -102,4 +102,14 @@ class Portfolio:
         print("Weights by Sector:")
         for sector, weight in self.weights_by_sector().items():
             print(f"{sector}: {weight:.2%}")
-        
+
+    def print_asset_table(self):
+        print("\nCurrent Portfolio Overview:")
+        print(f"{'Ticker':<10} {'Asset Class':<15} {'Sector':<30} {'Mkt Cap':<10} {'Qty':<5} {'Purchase Price':<15} {'Current Price':<15} {'Daily Return %':<15}")
+        print("-" * 115)
+
+        for a in self.assets:
+            market_cap = f"${int(a.market_cap / 1_000_000_000)}B" if isinstance(a.market_cap, (int, float)) else "N/A"
+            current_price = f"${a.close:.2f}" if a.close else "N/A"
+            daily_return = f"{a.daily_return:.2f}%" if a.daily_return is not None else "N/A"
+            print(f"{a.ticker:<10} {a.asset_class:<15} {a.sector:<30} {market_cap:<10} {a.quantity:<5} ${a.purchase_price:<14.2f} {current_price:<15} {daily_return:<15}")
